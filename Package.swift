5f3f64f941806194213b893644e13365300aff81
felipechaux <f.chaux@kanguroseguro.com>
2025-10-28 17:04:04 -0500

    Update Package.swift to use public repository
    - Changed repository URL from kanguroseguro/kanguro-shared-kmp to felipechaux/kanguro-shared-kmp
    - Repository is now public so XCFramework can be downloaded without authentication
    - iOS projects can now successfully import SharedKMP

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SharedKMP",
-            url: "https://github.com/kanguroseguro/kanguro-shared-kmp/releases/download/v1.0.1/SharedKMP.xcframework.zip",
+            url: "https://github.com/felipechaux/kanguro-shared-kmp/releases/download/v1.0.1/SharedKMP.xcframework.zip",
             checksum: "525cb802dc3abfc25b85827b16cbbb57e331f9ff3c50abf05a6f73f901be5eb9"
         )
     ]
